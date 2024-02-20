b0928bee3299b3068acd39b0895ac224691cca78
Marvin <mdengomg2@gmail.com>
2024-02-19 16:10:45 -0800

    feat: Add waiting to input and button click utility functions

--- utils/ScraperUtils.py
+++ utils/ScraperUtils.py
@@ -1,42 +1,47 @@
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 
 def enter_input_by_id(input_text: str, input_id: str, driver: WebDriver) -> None:
     """Enter info into an input bar given its id."""
     try:
-        email_input = driver.find_element(By.ID, input_id)
+        email_input = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.ID, input_id))
+        )
         email_input.clear()
         email_input.send_keys(input_text)
+    except TimeoutException:
+        print(
+            f"Timeout: Input with ID '{input_id}' not visible within the wait time.")
     except NoSuchElementException:
         print(f"Error: Input with ID '{input_id}' not found.")
 
 
 def click_button_by_css(css: str, driver: WebDriver) -> None:
-    """Click on the first button matching the css"""
+    """Click on the first button matching the css."""
     try:
-        button = driver.find_element(By.CSS_SELECTOR, css)
-        button.click()
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, css))).click()
+    except TimeoutException:
+        print(
+            f"Timeout: Button with css '{css}' not clickable within the wait time.")
     except NoSuchElementException:
-        print(f"Error: Button with css name '{css}' not found.")
+        print(f"Error: Button with css '{css}' not found.")
 
-
-def click_button_by_class(class_name: str, driver: WebDriver) -> None:
-    """Click on the first button matching the class name"""
-    try:
-        button = driver.find_element(By.CLASS_NAME, class_name)
-        button.click()
-    except NoSuchElementException:
-        print(f"Error: Button with class name '{class_name}' not found.")
 
 def click_element_by_data_test(data_test_value: str, driver: WebDriver) -> None:
     """Click on the first element matching the data-test attribute."""
     try:
         css_selector = f'[data-test="{data_test_value}"]'
-        element = driver.find_element(By.CSS_SELECTOR, css_selector)
-        element.click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, css_selector))).click()
+    except TimeoutException:
+        print(
+            f"Timeout: Element with data-test='{data_test_value}' not clickable within the wait time.")
     except NoSuchElementException:
         print(f"Error: Element with data-test='{data_test_value}' not found.")
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,1 @@
-from .ScraperUtils import export_formatted_html, enter_input_by_id, click_button_by_class, click_button_by_css, click_element_by_data_test
+from .ScraperUtils import export_formatted_html, enter_input_by_id, click_button_by_css, click_element_by_data_test
