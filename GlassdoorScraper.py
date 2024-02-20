a6f4f7a4a85f9c07e2bd6b7dad65b57f5058d654
Marvin <mdengomg2@gmail.com>
2024-02-19 16:17:13 -0800

    feat: Add function to bypass Glassdoor login

--- GlassdoorScraper.py
+++ GlassdoorScraper.py
@@ -1,7 +1,5 @@
 import os
-import time
 from typing import Optional
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -11,14 +9,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-from utils import export_formatted_html, enter_input_by_id, click_button_by_class, click_button_by_css, click_element_by_data_test
+from utils import export_formatted_html, enter_input_by_id, click_button_by_css, click_element_by_data_test
 
 
 class GlassdoorScraper:
     def __init__(self, url: str) -> None:
         self.url: str = url
         self.driver: Optional[WebDriver] = None
-        self.wait: Optional[WebDriverWait] = None
         self.page_source: Optional[str] = None
 
     def install_web_driver(self) -> None:
@@ -26,9 +23,9 @@ class GlassdoorScraper:
         try:
             self.driver = webdriver.Chrome(
                 service=Service(ChromeDriverManager().install()))
-            self.wait = WebDriverWait(self.driver, 5)
+            wait = WebDriverWait(self.driver, 5)
             self.driver.get(self.url)
-            self.wait.until(EC.url_to_be(self.url))
+            wait.until(EC.url_to_be(self.url))
             self.page_source = self.driver.page_source
             print("Page source obtained successfully.")
         except Exception as e:
@@ -41,14 +38,21 @@ class GlassdoorScraper:
 
     def get_company_overview(self):
         click_button_by_css("h3.d-none.d-sm-block", self.driver)
+        print("In company overview")
+
+    def get_interview_page(self) -> None:
+        """Enters into interview questions page"""
+        click_element_by_data_test("ei-nav-interviews-link", self.driver)
+        print("In interview page")
 
     def login_glassdoor(self) -> None:
         """Login to Glassdoor"""
         enter_input_by_id(os.getenv("EMAIL"), "hardsellUserEmail", self.driver)
-        click_button_by_class("ButtonContent", self.driver)
-
-    def get_interview_page(self) -> None:
-        """Enters into interview questions page"""
+        click_element_by_data_test("email-form-button", self.driver)
+        enter_input_by_id(os.getenv("PASSWORD"),
+                          "hardsellUserPassword", self.driver)
+        click_button_by_css("button.Button[type='submit']", self.driver)
+        print("Logged In!")
 
     @staticmethod
     def get_interview_questions(company: str) -> None:
@@ -56,9 +60,9 @@ class GlassdoorScraper:
         scraper = GlassdoorScraper(search_url)
         scraper.install_web_driver()
         scraper.get_company_overview()
-        # scraper.login_glassdoor()
-        time.sleep(10)
-        # scraper.close_driver()
+        scraper.get_interview_page()
+        scraper.login_glassdoor()
+        scraper.close_driver()
 
 
 if __name__ == "__main__":
