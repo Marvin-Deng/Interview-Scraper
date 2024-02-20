5a6e07464285245bb230101d5c0ec00c95c78bd7
Marvin <mdengomg2@gmail.com>
2024-02-19 20:30:57 -0800

    feat: Setup basic endpoint and initalized sqlite db

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,1 @@
-from .ScraperUtils import export_formatted_html, enter_input_by_id, click_button_by_css, click_element_by_data_test, get_elements_by_css
+from .scraper_utils import export_formatted_html, enter_input_by_id, click_button_by_css, click_element_by_data_test, get_elements_by_css
